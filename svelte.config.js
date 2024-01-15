a690c4f40290968702413385d2e224f8d8cc72f9
Darryl Yeo <darryl@ritual.net>
2024-01-15 12:16:00 -0800

    Svelte: add import alias for components directory

--- svelte.config.js
+++ svelte.config.js
@@ -15,5 +15,9 @@ export default {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
+
+		alias: {
+			'$components': './src/components',
+		},
 	},
 }
