ec33b078b9f96402ff7139d937edb9fb50efdce4
Darryl Yeo <darryl@ritual.net>
2024-01-15 14:33:03 -0800

    <Table>: set up basic table component using svelte-headless-table

---
+++ src/components/Table.svelte
@@ -0,0 +1,80 @@
+<script lang="ts">
+	// Types
+	type Datum = $$Generic<any>
+		
+	export let data: Datum[]
+	export let columns: Parameters<Table<Datum>['column']>[0][]
+			
+			
+	// Internal state
+	import { writable } from 'svelte/store'
+	import { createTable, Subscribe, Render, type Table, type Column } from 'svelte-headless-table'
+	
+	const _data = writable(data)
+	$: $_data = data
+
+	const table = createTable(_data)
+
+	const { headerRows, rows, tableAttrs, tableBodyAttrs } = table.createViewModel(
+		table.createColumns(
+			columns.map(table.column)
+		)
+	)
+</script>
+
+
+<div>
+	<table {...$tableAttrs}>
+		<thead>
+			{#each $headerRows as headerRow (headerRow.id)}
+				<Subscribe rowAttrs={headerRow.attrs()} let:rowAttrs>
+					<tr {...rowAttrs}>
+						{#each headerRow.cells as cell (cell.id)}
+							<Subscribe attrs={cell.attrs()} let:attrs>
+								<th {...attrs}>
+									<Render of={cell.render()} />
+								</th>
+							</Subscribe>
+						{/each}
+					</tr>
+				</Subscribe>
+			{/each}
+		</thead>
+
+		<tbody {...$tableBodyAttrs}>
+			{#each $rows as row (row.id)}
+				<Subscribe rowAttrs={row.attrs()} let:rowAttrs>
+					<tr {...rowAttrs}>
+						{#each row.cells as cell (cell.id)}
+							<Subscribe attrs={cell.attrs()} let:attrs>
+								<td {...attrs}>
+									<Render of={cell.render()} />
+								</td>
+							</Subscribe>
+						{/each}
+					</tr>
+				</Subscribe>
+			{/each}
+		</tbody>
+	</table>
+</div>
+
+
+<style>
+	table {
+		border-spacing: var(--border-width);
+		width: 100%;
+	}
+
+	tbody tr {
+		box-shadow: 0 var(--border-width) var(--border-color), 0 calc(-1 * var(--border-width)) var(--border-color);
+	}
+
+	th, td {
+		padding: 1em;
+	}
+
+	thead th {
+		opacity: 0.5;
+	}
+</style>
