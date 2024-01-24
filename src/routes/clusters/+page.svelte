cab67e8270556815214d0fc754189125684b5d2a
Darryl Yeo <darryl@ritual.net>
2024-01-23 17:54:01 -0800

    Clusters: add link to create cluster from main view

--- src/routes/clusters/+page.svelte
+++ src/routes/clusters/+page.svelte
@@ -11,16 +11,42 @@
 </script>
 
 
-<Table
-	data={clusters}
-	columns={[
-		{
-			header: 'Id',
-			accessor: d => d.id,
-		},
-		{
-			header: 'Name',
-			accessor: d => d.name,
-		},
-	]}
-/>
+<div class="column">
+	<header class="row">
+		<h2>Clusters</h2>
+
+		{#if clusters.length}
+			<button class="primary">
+				Create Cluster
+			</button>
+		{/if}
+	</header>
+
+	{#if clusters.length}
+		<section class="card">
+			<Table
+				data={clusters}
+				columns={[
+					{
+						header: 'Id',
+						accessor: d => d.id,
+					},
+					{
+						header: 'Name',
+						accessor: d => d.name,
+					},
+				]}
+			/>
+		</section>
+	{:else}
+		<div class="card column">
+			<p>You have not created any clusters.</p>
+
+			<a href="/clusters/create">
+				<button class="primary">
+					Create Cluster
+				</button>
+			</a>
+		</div>
+	{/if}
+</div>
