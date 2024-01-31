a1e5084e069db72d177031438c177e77538afaa0
Darryl Yeo <darryl@ritual.net>
2024-01-30 16:42:35 -0800

    Login: set up form

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,4 +1,17 @@
 <script lang="ts">
+	// Types
+	import type { PageData } from './$types'
+
+
+	// Internal state
+	import { page } from '$app/stores'
+	import { superForm } from 'sveltekit-superforms/client'
+
+	const { form, enhance, errors, constraints } = superForm(($page.data as PageData).form, {
+		dataType: 'json',
+	})
+
+
 	// Context
 	import { getContext } from 'svelte'
 
@@ -8,12 +21,25 @@
 
 {#if !$isSignedIn}
 	<form
+		class="card column"
 		on:submit={() => $isSignedIn = true}
 	>
+		<h3>Create account</h3>
+		<p>Set up your Infernet Cloud account to get started.</p>
+
+		<input
+			type="email"
+			name="email"
+			placeholder="Enter email address"
+			bind:value={$form.email}
+			{...$constraints.email}
+		/>
+
 		<button>Log in</button>
 	</form>
 {:else}
 	<form
+		class="card"
 		on:submit={() => $isSignedIn = false}
 	>
 		<button>Sign out</button>
