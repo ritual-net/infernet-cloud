dee8a7c53788f6d05320e0ca4902fac9dd4f2650
Darryl Yeo <darryl@ritual.net>
2024-01-31 13:11:42 -0800

    <Collapsible>: create collapsible component using Melt UI

---
+++ src/components/Collapsible.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { createCollapsible, melt } from '@melt-ui/svelte'
+	import { slide } from 'svelte/transition'
+
+
+	const {
+		elements: { root, content, trigger },
+		states: { open },
+	} = createCollapsible()
+</script>
+
+	
+<div use:melt={$root}>
+	<button
+		type="button"
+		use:melt={$trigger}
+	>
+		<slot name="trigger" />
+	</button>
+
+	{#if $open}
+		<div use:melt={$content} transition:slide>
+			<slot />
+		</div>
+	{/if}
+</div>
+
+
+<style>
+	button {
+		padding: none;
+		box-shadow: none;
+	}
+</style>
