389c19f65511ee4435c59f744d060b83d6c43c6c
Darryl Yeo <darryl@ritual.net>
2024-02-02 13:08:50 -0800

    <SizeTransition>: smoothly transition height of inner contents

---
+++ src/components/SizeTransition.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	// Internal state
+	let borderBoxSize: ResizeObserverSize[]
+</script>
+
+
+<div
+	class="size-transition"
+	style={`
+		--blockSize: ${borderBoxSize?.[0].blockSize}px;
+	`}
+>
+	<div bind:borderBoxSize>
+		<slot />
+	</div>
+</div>
+
+
+<style>
+	.size-transition {
+		contain: size;
+		contain-intrinsic-block-size: auto var(--blockSize);
+		transition: contain-intrinsic-block-size 0.3s var(--transition-easeOutExpo);
+	}
+</style>
