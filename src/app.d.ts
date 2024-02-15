f05dc6d6eb857da8df76c72d989b3c5af3c09a51
Darryl Yeo <darryl@ritual.net>
2024-02-15 14:17:01 -0800

    Auth: avoid blocking pages on auth – return whether signed in instead

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,17 +1,20 @@
-import type { Client } from 'edgedb';
+import type { Client } from 'edgedb'
 
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
+
 		interface Locals {
 			client: Client;
+			isSignedIn: boolean;
 		}
+
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
 	}
 }
 
-export {};
+export {}
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,5 @@
-import { createClient } from '$schema/edgeql-js';
-import { error, type Handle } from '@sveltejs/kit';
+import { createClient } from '$schema/edgeql-js'
+import { error, type Handle } from '@sveltejs/kit'
 
 /**
  * Global middleware for the server.
@@ -16,17 +16,18 @@ import { error, type Handle } from '@sveltejs/kit';
 export const handle: Handle = async ({ event, resolve }) =>{
 	// Allow requests to the auth server
 	if (event.url.pathname.startsWith('/auth')) {
-		return await resolve(event);
+		return await resolve(event)
 	}
 
 	// Extract token from cookie
-	const token = event.cookies.get('edgedb-auth-token');
-	if (!token) {
-		return error(
-			400,
-			'Could not find "token" in the cookie store. Is this the same user agent/browser that started the authorization flow?'
-		);
-	}
+	const token = event.cookies.get('edgedb-auth-token')
+	event.locals.isSignedIn = Boolean(token)
+	// if (!token) {
+	// 	return error(
+	// 		400,
+	// 		'Could not find "token" in the cookie store. Is this the same user agent/browser that started the authorization flow?'
+	// 	);
+	// }
 
 	// Attach client to the event
 	event.locals.client = createClient().withGlobals({
