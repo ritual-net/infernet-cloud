1569b1d7adb08f72dc406cf660c5612b615d2eea
Darryl Yeo <darryl@ritual.net>
2024-02-20 18:19:04 -0800

    API: fetch current signed-in user

---
+++ src/routes/api/user/+server.ts
@@ -0,0 +1,21 @@
+import { e } from '$/lib/db'
+import { json } from '@sveltejs/kit'
+import type { RequestHandler } from '@sveltejs/kit'
+
+/**
+ * Fetch the signed in user.
+ *
+ * @returns the current User object.
+ */
+export const GET: RequestHandler = async ({
+	locals: { client },
+}) => {
+	const user = await e
+		.select(e.global.current_user, () => ({
+			name: true,
+			email: true,
+		}))
+		.run(client);
+
+	return json(user)
+};
