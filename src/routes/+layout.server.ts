41715dba176fefbc73e38800e0b0e6a688eb888a
Darryl Yeo <darryl@ritual.net>
2024-02-20 18:19:27 -0800

    Global layout: fetch current user

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,9 +1,18 @@
-import type { ServerLoad } from "@sveltejs/kit"
+import type { ServerLoad } from '@sveltejs/kit'
 
-export const load: ServerLoad = ({
+export const load: ServerLoad = async ({
 	locals: { isSignedIn },
+	fetch,
 }) => {
+	const user = isSignedIn
+		? await fetch('/api/user')
+			.then(response => response.json())
+		: undefined
+
+	console.log({user})
+
 	return {
 		isSignedIn,
+		user,
 	}
 }
