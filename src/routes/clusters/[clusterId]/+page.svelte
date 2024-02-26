7dff20ed68bfadbbb1fdfe67f6522b4ec4f48fc5
Darryl Yeo <darryl@ritual.net>
2024-02-26 15:52:02 -0800

    Cluster: update layout

--- src/routes/clusters/[clusterId]/+page.svelte
+++ src/routes/clusters/[clusterId]/+page.svelte
@@ -6,6 +6,7 @@
 	$: ({
 		cluster
 	} = $page.data as PageData)
+$: console.log({cluster})
 
 
 	// Components
@@ -14,95 +15,104 @@
 </script>
 
 
-<header class="row wrap">
-	<div class="column">
-		<h3>
-			{cluster.name}
-		</h3>
+<div class="container column">
+	<header class="row wrap">
+		<div class="column">
+			<h3>
+				{cluster.name || cluster.id}
+			</h3>
 
-		<!-- <p>Created {cluster.created}</p> -->
-	</div>
-
-	<a
-		href={resolveRoute(`/clusters/[clusterId]/edit`, {
-			clusterId: $page.params.clusterId,
-		})}
-		class="button"
-	>Edit</a>
-</header>
-
-<section class="column">
-	<h3>Details</h3>
-
-	<dl class="card">
-		<div class="section">
-			<dt>Service Account</dt>
-
-			<dd>
-				<a
-					href={resolveRoute(`/service-accounts/[serviceAccountId]`, {
-						serviceAccountId: cluster.service_account.id,
-					})}
-				>
-					{cluster.service_account.name}
-				</a>
-			</dd>
+			<!-- <p>Created {cluster.created}</p> -->
 		</div>
 
-		<div class="section">
-			<dt>IPs Allowed (HTTP)</dt>
+		<a
+			href={resolveRoute(`/clusters/[clusterId]/edit`, {
+				clusterId: $page.params.clusterId,
+			})}
+			class="button"
+		>Edit</a>
+	</header>
 
-			{#if cluster.ip_allow_http?.length}
-				<dd class="column inline">
-					{#each cluster.ip_allow_http as ip}
-						{ip}
-					{/each}
+	<section class="column">
+		<h3>Details</h3>
+
+		<dl class="card">
+			<section class="row">
+				<dt>Service Account</dt>
+
+				<dd>
+					<a
+						href={resolveRoute(`/service-accounts/[serviceAccountId]`, {
+							serviceAccountId: cluster.service_account.id,
+						})}
+					>
+						{cluster.service_account.name}
+					</a>
 				</dd>
-			{:else}
-				All
-			{/if}
-		</div>
+			</section>
 
-		<div class="section">
-			<dt>Deploy Router</dt>
+			<section class="row">
+				<dt>IPs Allowed (HTTP)</dt>
 
-			<dd>
-				{cluster.deploy_router ? 'Yes' : 'No'}
-			</dd>
-		</div>
-
-		<div class="section">
-			<dt>Region / Zone</dt>
-
-			<dd>
-				{#if 'region' in cluster}
-					{cluster.region}
+				{#if cluster.ip_allow_http?.length}
+					<dd class="column inline">
+						{#each cluster.ip_allow_http as ip}
+							{ip}
+						{/each}
+					</dd>
+				{:else}
+					All
 				{/if}
+			</section>
 
-				{#if 'region' in cluster && 'zone' in cluster}
-					/
-				{/if}
+			<section class="row">
+				<dt>Deploy Router</dt>
 
-				{#if 'zone' in cluster}
-					{cluster.zone}
-				{/if}
-			</dd>
-		</div>
+				<dd>
+					{cluster.deploy_router ? 'Yes' : 'No'}
+				</dd>
+			</section>
 
-		<div class="section">
-			<dt>Machine Type</dt>
+			<section class="row">
+				<dt>Region / Zone</dt>
 
-			<dd>
-				{cluster.machine_type}
-			</dd>
-		</div>
-	</dl>
-</section>
+				<dd>
+					{#if 'region' in cluster}
+						{cluster.region}
+					{/if}
 
-<div>
-	<h3>Nodes</h3>
+					{#if 'region' in cluster && 'zone' in cluster}
+						/
+					{/if}
 
-	<NodesTable
-		nodes={cluster.nodes}
-	/>
+					{#if 'zone' in cluster}
+						{cluster.zone}
+					{/if}
+				</dd>
+			</section>
+
+			<section class="row">
+				<dt>Machine Type</dt>
+
+				<dd>
+					{cluster.machine_type}
+				</dd>
+			</section>
+		</dl>
+	</section>
+
+	<div>
+		<h3>Nodes</h3>
+
+		<NodesTable
+			nodes={cluster.nodes}
+		/>
+	</div>
 </div>
+
+
+<style>
+	.container {
+		gap: 2rem;
+	}
+</style>
