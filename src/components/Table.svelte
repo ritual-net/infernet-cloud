353a1a482b4e0c4db027ee3ad5a2a71e43b7f02d
Darryl Yeo <darryl@ritual.net>
2024-03-12 16:10:11 -0700

    <Table>: improve layout for menu column

--- src/components/Table.svelte
+++ src/components/Table.svelte
@@ -74,6 +74,7 @@
 							<th
 								class="sticky"
 								data-align="end"
+								data-column="menu"
 							>
 								Actions
 							</th>
@@ -126,11 +127,14 @@
 								<td
 									class="sticky"
 									data-align="end"
+									data-column="menu"
 								>
 									<DropdownMenu
 										items={contextMenu(datum)}
 									>
-										• • •
+										<svg width="14" height="14" viewBox="-5 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+											<path d="M2 0C2.39782 0 2.77936 0.158035 3.06066 0.43934C3.34196 0.720644 3.5 1.10218 3.5 1.5C3.5 1.89782 3.34196 2.27936 3.06066 2.56066C2.77936 2.84196 2.39782 3 2 3C1.60218 3 1.22064 2.84196 0.93934 2.56066C0.658035 2.27936 0.5 1.89782 0.5 1.5C0.5 1.10218 0.658035 0.720644 0.93934 0.43934C1.22064 0.158035 1.60218 0 2 0ZM2 5.5C2.39782 5.5 2.77936 5.65804 3.06066 5.93934C3.34196 6.22064 3.5 6.60218 3.5 7C3.5 7.39782 3.34196 7.77936 3.06066 8.06066C2.77936 8.34196 2.39782 8.5 2 8.5C1.60218 8.5 1.22064 8.34196 0.93934 8.06066C0.658035 7.77936 0.5 7.39782 0.5 7C0.5 6.60218 0.658035 6.22064 0.93934 5.93934C1.22064 5.65804 1.60218 5.5 2 5.5ZM3.5 12.5C3.5 12.1022 3.34196 11.7206 3.06066 11.4393C2.77936 11.158 2.39782 11 2 11C1.60218 11 1.22064 11.158 0.93934 11.4393C0.658035 11.7206 0.5 12.1022 0.5 12.5C0.5 12.8978 0.658035 13.2794 0.93934 13.5607C1.22064 13.842 1.60218 14 2 14C2.39782 14 2.77936 13.842 3.06066 13.5607C3.34196 13.2794 3.5 12.8978 3.5 12.5Z" fill="black"/>
+										</svg>
 									</DropdownMenu>
 								</td>
 							{/if}
@@ -217,15 +221,6 @@
 	th, td {
 		padding: 1em;
 
-		&.sticky {
-			position: sticky;
-			backdrop-filter: blur(20px);
-
-			&:last-child {
-				inset-inline-end: 0;
-			}
-		}
-
 		&[data-align="start"] {
 			text-align: start;
 			align-items: start;
@@ -235,6 +230,27 @@
 			text-align: end;
 			align-items: end;
 			transform-origin: right;
+		}
+
+		&.sticky {
+			position: sticky;
+			backdrop-filter: blur(20px);
+
+			&:last-child {
+				inset-inline-end: 0;
+			}
+		}
+	}
+
+	th[data-column="menu"] {
+		width: 0;
+	}
+	td[data-column="menu"] {
+		padding: 0;
+
+		> :global(*) {
+			padding: 1em;
+			height: 100%;
 		}
 	}
 </style>
