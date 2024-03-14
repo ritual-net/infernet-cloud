574125c8472a3387a9187055cdb72d2ac2b3040e
Stelios Rousoglou <stelios@ritual.net>
2024-03-14 13:50:02 -0400

    add files

--- dbschema/interfaces.ts
+++ dbschema/interfaces.ts
@@ -27,7 +27,7 @@ export interface Cluster extends std.$Object {
 	ip_allow_ssh: string[];
 	locked: boolean;
 	name: string;
-	router?: { id: string; ip: string } | null;
+	router_ip?: string | null;
 	tfstate?: string | null;
 }
 export interface AWSCluster extends Cluster {
