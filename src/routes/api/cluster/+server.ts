77069367e51055b3206147301478529c7a14822c
Darryl Yeo <darryl@ritual.net>
2024-03-20 11:12:05 -0700

    Cluster create: improve backend error handling

--- src/routes/api/cluster/+server.ts
+++ src/routes/api/cluster/+server.ts
@@ -40,9 +40,11 @@ export const POST: RequestHandler = async ({ locals: { client }, request }) => {
 		return error(400, 'Service account could not be retrieved');
 	}
 
+	let cluster: Cluster
+
 	try {
 		// Insert cluster
-		const cluster = (await e
+		cluster = (await e
 			.params(
 				{
 					nodes: e.array(createNodeParams),
@@ -58,19 +60,37 @@ export const POST: RequestHandler = async ({ locals: { client }, request }) => {
 					})
 			)
 			.run(client, { nodes })) as Cluster;
+	} catch (e) {
+		console.error(e)
 
+		if(e.message?.includes(`violates exclusivity constraint`)){
+			return error(500, `A cluster with name "${config.name}" already exists.`)
+		}
+
+		return error(500, (e as Error).message);
+	}
+
+	let result: Awaited<ReturnType<typeof clusterAction>>
+
+	try {
 		// Apply Terraform changes to create cluster
-		const { error: errorMessage, success } = await clusterAction(
+		result = await clusterAction(
 			client,
 			cluster.id,
 			TFAction.Apply
 		);
-		return json({
-			id: cluster.id,
-			message: success ? 'Cluster created successfully' : errorMessage,
-			success,
-		});
 	} catch (e) {
-		return error(500, (e as Error).message);
+		console.error(e)
+
+		return error(500, JSON.stringify(e))
 	}
+
+	const { success, error: errorMessage } = result
+
+	if(!success)
+		return error(500, errorMessage)
+
+	return json({
+		cluster,
+	})
 };
