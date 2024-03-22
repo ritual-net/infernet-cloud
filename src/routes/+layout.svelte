3d2e4daf0206914209fca03ab3f9ce9f5cb5fe5b
Darryl Yeo <darryl@ritual.net>
2024-03-21 22:52:54 -0700

    Tanstack Query: install

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,19 @@
 
 	// Context
 	import { page } from '$app/stores'
+	import { browser } from '$app/environment'
+
+
+	// Global state
+	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query'
+
+	const queryClient = new QueryClient({
+		defaultOptions: {
+			queries: {
+				enabled: browser,
+			},
+		},
+	})
 
 
 	// Components
@@ -14,19 +27,21 @@
 </script>
 
 
-<header>
-	<Nav />
-</header>
+<QueryClientProvider client={queryClient}>
+	<header>
+		<Nav />
+	</header>
 
-<div class="main-wrapper">
-	<main>
-		<slot />
-	</main>
-</div>
+	<div class="main-wrapper">
+		<main>
+			<slot />
+		</main>
+	</div>
 
-<!-- <footer></footer> -->
+	<!-- <footer></footer> -->
 
-<Toaster />
+	<Toaster />
+</QueryClientProvider>
 
 
 <style>
