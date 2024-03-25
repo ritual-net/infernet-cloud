3ed2aa7509980f3f004fb31d354c74b7c616b3c5
Darryl Yeo <darryl@ritual.net>
2024-03-25 12:26:47 -0700

    Cluster overview: add endpoint + action to apply Terraform changes to cluster

--- src/routes/(signedIn)/clusters/[clusterId]/+page.server.ts
+++ src/routes/(signedIn)/clusters/[clusterId]/+page.server.ts
@@ -1,9 +1,49 @@
 // Actions
-import { type Actions, fail } from '@sveltejs/kit'
+import { type Actions } from '@sveltejs/kit'
 import { resolveRoute } from '$app/paths'
 import { message } from 'sveltekit-superforms/server'
 
 export const actions: Actions = {
+	apply: async ({
+		fetch,
+		params: { clusterId },
+	}) => {
+		const response = await fetch(
+			resolveRoute('/api/cluster/[clusterId]', { clusterId }),
+			{
+				method: 'POST',
+			},
+		)
+
+		if(!response.ok){
+			const result = await response.json()
+
+			return message(
+				{},
+				{
+					title: `Couldn't apply changes to cluster.`,
+					description: result,
+				},
+				{
+					status: response.status,
+				}
+			)
+		}
+
+		const result = await response.json()
+
+		return message(
+			{},
+			{
+				title: `Applied changes to cluster.`,
+				description: result,
+			},
+			{
+				status: response.status,
+			}
+		)
+	},
+
 	delete: async ({
 		fetch,
 		params: { clusterId },
