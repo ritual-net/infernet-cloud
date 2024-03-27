a3bfc80f33580531615a01c5795a6663c58220a0
Darryl Yeo <darryl@ritual.net>
2024-03-26 18:35:38 -0700

    Cluster create: await imagesPromise to allow pushState serialization

--- src/routes/(signedIn)/clusters/create/+page.svelte
+++ src/routes/(signedIn)/clusters/create/+page.svelte
@@ -767,7 +767,10 @@
 												pushState(href, {
 													showContainerForm: 'create',
 													nodeId: node.id,
-													containerFormData: result.data,
+													containerFormData: {
+														...result.data,
+														imagesPromise: await result.data.imagesPromise,
+													}
 												})
 											} else {
 												console.error(`Failed to preload shallow route: ${href}`)
@@ -794,7 +797,7 @@
 												formData: {
 													container,
 												},
-												imagesPromise,
+												imagesPromise: await imagesPromise,
 											},
 										}
 
