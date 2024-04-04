724c37017438fdd7d666dd92957a2d5311d6b664
Darryl Yeo <darryl@ritual.net>
2024-04-03 17:11:48 -0700

    Node: invalidate every 5 seconds

---
+++ src/routes/(signedIn)/nodes/[nodeId]/+layout.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	// Context
+	import { page } from '$app/stores'
+	
+	
+	// Actions
+	import { invalidate } from '$app/navigation'
+	import { onMount } from 'svelte'
+	import { resolveRoute } from '$app/paths'
+
+	onMount(() => {
+		let isMounted = true
+
+		;(async () => {
+			while(isMounted) {
+				await new Promise(resolve => setTimeout(resolve, 5000))
+				if(!isMounted) return
+
+				await invalidate(
+					resolveRoute('/api/node/[nodeId]', {
+						nodeId: $page.params.nodeId,
+					})
+				)
+			}
+		})()
+
+		return () => {
+			isMounted = false
+		}
+	})
+</script>
