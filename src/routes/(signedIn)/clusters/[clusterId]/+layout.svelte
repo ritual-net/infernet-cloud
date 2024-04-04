c473a19455293ecfe48eeab3ba983d044e3fe313
Darryl Yeo <darryl@ritual.net>
2024-04-03 17:04:26 -0700

    Cluster: invalidate every 5 seconds

--- src/routes/(signedIn)/clusters/[clusterId]/+layout.svelte
+++ src/routes/(signedIn)/clusters/[clusterId]/+layout.svelte
@@ -1,3 +1,32 @@
+<script lang="ts">
+	// Context
+	import { page } from '$app/stores'
+	
+	
+	// Actions
+	import { invalidate } from '$app/navigation'
+	import { onMount } from 'svelte'
+	import { resolveRoute } from '$app/paths'
+
+	onMount(() => {
+		let isMounted = true
+
+		;(async () => {
+			while(isMounted) {
+				await new Promise(resolve => setTimeout(resolve, 5000))
+				if(!isMounted) return
+
+				await invalidate(resolveRoute('/api/cluster/[clusterId]', { clusterId: $page.params.clusterId }))
+			}
+		})()
+
+		return () => {
+			isMounted = false
+		}
+	})
+</script>
+
+
 <nav class="breadcrumb">
 	<a
 		href="/clusters"
