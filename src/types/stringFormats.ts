ca1a926db4fc798f7b8f4a305d3ee257c653e6d4
Darryl Yeo <darryl@ritual.net>
2024-04-26 11:28:27 -0700

    stringFormats: use non-global regex

--- src/types/stringFormats.ts
+++ src/types/stringFormats.ts
@@ -2,15 +2,15 @@ import * as z from 'yup'
 
 export const Ip = z
 	.string<`${number}.${number}.${number}.${number}/${number}`>()
-	.matches(/^\d+\.\d+\.\d+\.\d+\$/g)
+	.matches(/^\d+\.\d+\.\d+\.\d+\$/)
 	.defined()
 
 export const IpWithAddressMask = z
 	.string<`${number}.${number}.${number}.${number}/${number}`>()
-	.matches(/^\d+\.\d+\.\d+\.\d+\/\d+$/g)
+	.matches(/^\d+\.\d+\.\d+\.\d+\/\d+$/)
 	.defined()
 
 export const Address = z
 	.string<`0x${string}`>()
-	.matches(/^0x[0-9a-f]{40}$/gi)
+	.matches(/^0x[0-9a-f]{40}$/i)
 	.defined()
