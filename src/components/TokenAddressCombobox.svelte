c5ffac086060de88dfa782a48c36515c4d0a10ec
Darryl Yeo <darryl@ritual.net>
2024-06-12 15:27:46 -0700

    <TokenAddressCombobox>: select from provided tokens or input custom address

---
+++ src/components/TokenAddressCombobox.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	// Types
+	import { Address } from '$/types/stringFormats'
+	import type { Token } from '$/lib/tokens'
+
+
+	// Inputs
+	export let tokens: Token[] | undefined
+	export let value: `0x${string}` | undefined
+
+
+	// Functions
+	const isAddress = (address: string) => (
+		Address.isValidSync(address)
+	)
+
+
+	// Internal state
+	let inputValue = ''
+
+
+	// Components
+	import Combobox from '$/components/Combobox.svelte'
+</script>
+
+
+<Combobox
+	bind:value
+	bind:inputValue
+	placeholder={
+		tokens?.length
+			? `Choose token or enter address...`
+			: `Enter token address...`
+	}		
+	{...$$restProps}
+	items={[
+		tokens?.length && {
+			value: 'common',
+			label: 'Common',
+			items: tokens.map(token => ({
+				value: token.address,
+				label: token.name,
+				icon: token.icon,
+			})),
+		},
+		(
+			isAddress(inputValue)
+			&& !tokens?.some(token => token.address.toLowerCase() === inputValue.trim().toLowerCase())
+		) && {
+			value: 'custom',
+			label: 'Custom',
+			items: [
+				{
+					value: inputValue.trim(),
+					label: inputValue.trim(),
+				}
+			].filter(Boolean),
+		},
+	].filter(Boolean)}
+/>
