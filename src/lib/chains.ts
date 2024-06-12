4c3e6e2ee0c28f5d88086aacfdaad8c825561e16
Darryl Yeo <darryl@ritual.net>
2024-06-12 12:12:16 -0700

    Chains: add icons, testnet flag

--- src/lib/chains.ts
+++ src/lib/chains.ts
@@ -1,20 +1,34 @@
+// Types
 export type Chain = {
 	name: string
 	chainId: number
+	icon?: string
+	isTestnet?: boolean
 }
 
+
+// Components
+import ETHIcon from '$/icons/tokens/ETH.svg'
+import BaseIcon from '$/icons/networks/Base.svg'
+
+
+// Constants
 export const chains = [
 	{
 		name: 'Ethereum',
 		chainId: 1,
+		icon: ETHIcon,
 	},
 	{
 		name: 'Base',
 		chainId: 8453,
+		icon: BaseIcon,
 	},
 	{
 		name: 'Base Sepolia',
 		chainId: 84532,
+		icon: BaseIcon,
+		isTestnet: true,
 	},
 ] as const satisfies Chain[]
 
