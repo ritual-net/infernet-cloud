9f223d68ddef50f793b9a108ca85a8c5312fe434
Darryl Yeo <darryl@ritual.net>
2024-08-05 17:03:12 -0500

    <WithIcon>: render icon in row layout

---
+++ src/components/WithIcon.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	// Inputs
+	export let icon: string
+	export let alt: string | undefined
+	export let value: string
+</script>
+
+
+<div class="row">
+	<img
+		src={icon}
+		{alt}
+	/>
+	{value}
+</div>
+
+
+<style>
+	.row {
+		justify-content: start;
+	}
+
+	img {
+		width: 1.5em;
+		height: 1.5em;
+	}
+</style>
