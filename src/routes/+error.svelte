9a24804be36461158b2f42351429a9e61cf565f3
Darryl Yeo <darryl@ritual.net>
2024-08-08 12:22:40 -0500

    Error page: check for existence of error

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -12,7 +12,7 @@
 	<h3>There was a problem loading the page.</h3>
 
 	<div class="card column inine">
-		<p><code>{$page.error.message}</code></p>
+		<p><code>{$page.error?.message}</code></p>
 
 		<p>Status: {JSON.stringify($page.status)}</p>
 	</div>
