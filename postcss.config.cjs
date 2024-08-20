15e0a1691844fef7fa9191fa3474934dbac87fe1
Darryl Yeo <darryl@ritual.net>
2024-08-20 16:02:49 -0500

    PostCSS: install Autoprefixer

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,9 +1,11 @@
 const postcssNesting = require('postcss-nesting')
+const autoprefixer = require('autoprefixer')
 
 module.exports = {
 	plugins: [
 		postcssNesting({
 			edition: '2024-02',
 		}),
+		autoprefixer,
 	]
 }
