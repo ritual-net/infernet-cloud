12e382e95664da7215d27e6f17290d7464ae4548
Darryl Yeo <darryl@ritual.net>
2024-08-27 22:46:55 -0500

    Cluster edit: format

--- src/routes/(signedIn)/clusters/[clusterId]/edit/+page.ts
+++ src/routes/(signedIn)/clusters/[clusterId]/edit/+page.ts
@@ -12,21 +12,24 @@ export const load: PageLoad = async ({
 }) => {
 	const { cluster } = await parent()
 
-	const formData = await superValidate({
-		config: {
-			name: cluster.name,
-			deploy_router: Boolean(cluster.router),
-			ip_allow_http: cluster.ip_allow_http,
-			ip_allow_ssh: cluster.ip_allow_ssh,
-			region: cluster.region,
-			zone: cluster.zone,
+	const formData = await superValidate(
+		{
+			config: {
+				name: cluster.name,
+				deploy_router: Boolean(cluster.router),
+				ip_allow_http: cluster.ip_allow_http,
+				ip_allow_ssh: cluster.ip_allow_ssh,
+				region: cluster.region,
+				zone: cluster.zone,
+			},
+			router: {
+				region: cluster.router?.region,
+				zone: cluster.router?.zone,
+				machine_type: cluster.router?.machine_type,
+			}
 		},
-		router: {
-			region: cluster.router?.region,
-			zone: cluster.router?.zone,
-			machine_type: cluster.router?.machine_type,
-		}
-	}, yup(FormData))
+		yup(FormData)
+	)
 
 	return {
 		cluster,
