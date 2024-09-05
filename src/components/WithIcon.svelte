09d1fad8b203745ba3f04258f98e29abef446019
Darryl Yeo <darryl@ritual.net>
2024-09-05 13:25:10 -0500

    <WithIcon>: support icon as slot

--- src/components/WithIcon.svelte
+++ src/components/WithIcon.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
 	// Inputs
-	export let icon: string
+	export let icon: string | undefined
 	export let alt: string | undefined
 	export let value: string | undefined
 </script>
 
 
 <div class="row">
-	<img
-		src={icon}
-		{alt}
-	/>
+	<slot name="icon">
+		<img
+			src={icon}
+			{alt}
+		/>
+	</slot>
 
 	<slot>
 		{value}
@@ -22,10 +24,11 @@
 	.row {
 		justify-content: start;
 		gap: 0.66em;
-	}
 
-	img {
-		width: 1.5em;
-		height: 1.5em;
+		img,
+		:global(svg) {
+			width: 1.5em;
+			height: 1.5em;
+		}
 	}
 </style>
