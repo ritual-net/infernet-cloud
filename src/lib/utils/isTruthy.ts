49766ce6a1d32d3c9ed7dcd249b1f267b102d718
Darryl Yeo <darryl@ritual.net>
2024-09-26 21:50:23 -0500

    isTruthy: improve type predicate

--- src/lib/utils/isTruthy.ts
+++ src/lib/utils/isTruthy.ts
@@ -1,3 +1,3 @@
-export const isTruthy = <T>(value: T): value is NonNullable<T> => (
+export const isTruthy = <T>(value: T): value is Exclude<T, 0 | '' | false | null | undefined> => (
 	Boolean(value)
 )
